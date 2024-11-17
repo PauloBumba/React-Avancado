a2ea5cb2ebb71383d8ac425ae3a1f4cb8c59fa80
Paulo Mário Valente Bumba <143729787+PauloBumba@users.noreply.github.com>
2024-11-17 16:57:19 -0300

    Começando a modelagem do React

---
+++ First-project/src/App.tsx
@@ -0,0 +1,26 @@
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='container mt-5 '>
+        <div className='card'>
+          <div className='card-header fs-2 text-center'>Paulo Mário  </div>
+          
+          <div className="card-body">
+          <div className="card-subtitle">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit
+          </div>
+          <div className="card-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere excepturi, ipsa numquam sequi architecto incidunt alias ad deserunt impedit maxime autem necessitatibus porro quidem quasi inventore cumque aliquid tempore dignissimos?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem earum sed quaerat doloribus in inventore provident omnis, officia quos reprehenderit id aut excepturi, maxime quasi et. Perspiciatis pariatur placeat recusandae?</div>      
+                   
+          </div>
+        </div>
+       </div>
+    </>
+  )
+}
+
+export default App
